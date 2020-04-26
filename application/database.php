66efe70903f41250ec32673580a72e83f464ebab
shenlink <2024133718@qq.com>
2020-04-26 18:46:40 +0800

    更改了数据库配置，换了一个数据库

--- application/database.php
+++ application/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'shen',
+    'database'        => 'blog',
     // 用户名
-    'username'        => 'shen',
+    'username'        => 'blog',
     // 密码
-    'password'        => 'shen1004',
+    'password'        => 'blog1234',
     // 端口
     'hostport'        => '',
     // 连接dsn
