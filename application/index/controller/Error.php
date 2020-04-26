7fe2682fd42884e10ec257f813557f3bc0e2d76a
shenlink <2024133718@qq.com>
2020-04-26 12:31:26 +0800

    创建一个Error控制器，当用户输入不存在的控制器，会提示用户：‘url地址出错了’

---
+++ application/index/controller/Error.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\index\controller;
+
+class Error
+{
+    public function index()
+    {
+        return 'url地址出错了';
+    }
+
+    public function _empty()
+    {
+        return 'url地址出错了';
+    }
+}
