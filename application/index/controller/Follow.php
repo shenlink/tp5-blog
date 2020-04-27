c22022200ea3145a4d27f1a7d92a2b94a223b27a
shenlink <2024133718@qq.com>
2020-04-27 15:56:07 +0800

    完成了确认关注的功能

---
+++ application/index/controller/Follow.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace app\index\controller;
+
+use app\index\controller\Base;
+use app\index\model\Follow as FollowModel;
+use think\Request;
+
+class Follow extends Base
+{
+    // 确认关注
+    public function checkFollow(Request $request)
+    {
+        $data = $request->post();
+        $author = $data['author'];
+        $data['username'] = $this->username;
+        $result = FollowModel::get(['username' => $this->username, 'author' => $author]);
+        if ($result) {
+            $status = -11;
+            $message = '取消失败';
+            $cancel = FollowModel::destroy(['username' => $this->username, 'author' => $author]);
+            if ($cancel == true) {
+                $status = 11;
+                $message = '取消关注';
+                return ['status' => $status, 'message' => $message];
+            } else {
+                return ['status' => $status, 'message' => $message];
+            }
+        } else {
+            $status = 0;
+            $message = '关注失败';
+            $add = FollowModel::create($data);
+            if ($add == true) {
+                $status = 1;
+                $message = '关注成功';
+                return ['status' => $status, 'message' => $message];
+            } else {
+                return ['status' => $status, 'message' => $message];
+            }
+        }
+    }
+}
