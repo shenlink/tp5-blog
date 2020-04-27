421dc1aa0929976985500e8ca5bfe086bb86d5dd
shenlink <2024133718@qq.com>
2020-04-27 20:28:42 +0800

    把除了search.js的js文件中关于搜索的代码都删除了

--- public/static/js/write.js
+++ public/static/js/write.js
@@ -1,37 +1,3 @@
-// 搜索
-$('#search').on('click', function () {
-    let type = $('#type').val();
-    if (type == '2') {
-        searchContent = encodeURIComponent($('#searchContent').val());
-        if (searchContent == '') {
-            layer.msg('查询条件不能为空', {
-                time: 1000
-            });
-            return;
-        }
-    } else {
-        searchContent = $('#searchContent').val();
-        if (searchContent == '') {
-            layer.msg('查询条件不能为空', {
-                time: 1000
-            });
-            return;
-        }
-        let reg = /^(?=.*[a-z])[a-z0-9]{4,16}$/i;
-        if (!reg.test(searchContent)) {
-            layer.msg('用户名不符合要求', {
-                time: 1000
-            });
-            return;
-        }
-    }
-    if (type == '1') {
-        window.location.href = `/user/search/${searchContent}/1.html`;
-    } else {
-        window.location.href = `/article/search/${searchContent}/1.html`;
-    }
-});
-
 // 发表文章
 let E = window.wangEditor;
 let editor = new E('#content');
