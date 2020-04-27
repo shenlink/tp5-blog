a916084b2c2c36de69179f90c003bd3584231e44
shenlink <2024133718@qq.com>
2020-04-27 10:23:23 +0800

    完成了处理添加分类页面提交的数据的功能

--- application/index/controller/Category.php
+++ application/index/controller/Category.php
@@ -5,9 +5,34 @@ namespace app\index\controller;
 use app\index\controller\Base;
 use app\index\model\Category as CategoryModel;
 use app\index\model\Article;
+use think\Request;
 
 class Category extends Base
 {
+    // 添加分类
+    public function addCategory()
+    {
+        $addCategory = 'addCategory';
+        $recommends = Article::where('status', 1)->field(['article_id', 'title'])->limit(10)->order('comment_count', 'desc')->select();
+        $this->view->assign('addCategory', $addCategory);
+        $this->view->assign('recommends', $recommends);
+        return $this->view->fetch('public/add');
+    }
+
+    // 确认添加
+    public function checkAddCategory(Request $request)
+    {
+        $status = 0;
+        $message = '添加失败';
+        $data = $request->post();
+        $result = CategoryModel::create($data);
+        if ($result == true) {
+            $status = 1;
+            $message = '添加成功';
+        }
+        return ['status' => $status, 'message' => $message];
+    }
+
     public function _empty($category)
     {
         $result = CategoryModel::get(['category' => $category]);
