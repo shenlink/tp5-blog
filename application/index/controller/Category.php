61aa4826d0aee5223e038ae07f49a03eaddfaf0d
shenlink <2024133718@qq.com>
2020-04-27 19:50:45 +0800

    对分类页面的文章进行了分页

--- application/index/controller/Category.php
+++ application/index/controller/Category.php
@@ -39,7 +39,7 @@ class Category extends Base
         if (!$result) {
             $this->error('分类不存在', '/');
         }
-        $articles = Article::all(['category' => $category]);
+        $articles = Article::where(['category' => $category])->paginate(5);
         $recommends = Article::where('status', 1)->field(['article_id', 'title'])->limit(10)->order('comment_count', 'desc')->select();
         $this->view->assign('articles', $articles);
         $this->view->assign('category', $category);
