1c605cd137bc981396b78ab49f8815db2820ca05
shenlink <2024133718@qq.com>
2020-04-28 14:59:05 +0800

    文章按照时间降序排序

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -12,7 +12,7 @@ class Index extends Base
     public function index()
     {
         $announcements = Announcement::all();
-        $articles = Article::paginate(5);
+        $articles = Article::order('update_time desc')->paginate(5);
         $recommends = Article::where('status', 1)->field(['id', 'title'])->limit(10)->order('comment_count', 'desc')->select();
         $this->view->assign('announcements', $announcements);
         $this->view->assign('articles', $articles);
