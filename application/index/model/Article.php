640977763d55f8524b77a7080ad06e589badba70
shenlink <2024133718@qq.com>
2020-04-28 15:38:41 +0800

    给status字段用上获取器

--- application/index/model/Article.php
+++ application/index/model/Article.php
@@ -11,4 +11,13 @@ class Article extends Model
     protected $createTime = 'create_time';
     // 更新时间字段
     protected $updateTime = 'update_time';
+    //状态字段:status返回值处理
+    public function getStatusAttr($value)
+    {
+        $status = [
+            0 => '拉黑',
+            1 => '正常'
+        ];
+        return $status[$value];
+    }
 }
