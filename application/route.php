285d3033972c2e90dbe0122decb4dc046e7a3c10
shenlink <2024133718@qq.com>
2020-04-28 11:39:53 +0800

    增加了user控制器的路由，因为路由只能传一个参数，所有之前的不符合要求的参数都删除了

--- application/route.php
+++ application/route.php
@@ -25,57 +25,55 @@ Route::rule('article/search/:condition', 'article/search');
 
 Route::rule('announcement/checkAddAnnouncement/:content', '@index/announcement/checkAddAnnouncement', 'post');
 
-Route::rule('announcement/checkChange/:content/:id', '@index/announcement/checkAddAnnouncement', 'post');
+Route::rule('announcement/checkChange', '@index/announcement/checkAddAnnouncement', 'post');
 
 Route::rule('announcement/delAnnouncement/:id', '@index/announcement/delAnnouncement', 'post');
 
-Route::rule('article/checkWrite/:title/:content/:category', '@index/article/checkWrite', 'post');
+Route::rule('article/checkWrite', '@index/article/checkWrite', 'post');
 
-Route::rule('article/checkEdit/:id/:title/:content/:category', '@index/article/checkEdit', 'post');
+Route::rule('article/checkEdit', '@index/article/checkEdit', 'post');
 
 Route::rule('article/defriendArticle/:id', '@index/article/defriendArticle', 'post');
 
 Route::rule('article/normalArticle/:id', '@index/article/normalArticle', 'post');
 
-Route::rule('article/delArticle/:id/:category', '@index/article/delArticle', 'post');
+Route::rule('article/delArticle', '@index/article/delArticle', 'post');
 
 Route::rule('category/checkAddCategory/:category', '@index/category/checkAddCategory', 'post');
 
-Route::rule('collect/checkCollect/:id/:author/:title', '@index/collect/checkCollect', 'post');
+Route::rule('collect/checkCollect', '@index/collect/checkCollect', 'post');
 
-Route::rule('collect/delCollect/:article_id/:id', '@index/collect/delCollect', 'post');
+Route::rule('collect/delCollect', '@index/collect/delCollect', 'post');
 
-Route::rule('comment/addComment/:id/:author/:title/:content', '@index/comment/addComment', 'post');
+Route::rule('comment/addComment', '@index/comment/addComment', 'post');
 
-Route::rule('comment/delComment/:article_id/:id', '@index/comment/delComment', 'post', ['id' => '\d+', 'id' => '\d+']);
+Route::rule('comment/delComment', '@index/comment/delComment', 'post');
 
 Route::rule('follow/checkFollow/:author', '@index/@index/follow/checkFollow', 'post');
 
 Route::rule('follow/delFollow/:author', '@index/@index/follow/delFollow', 'post');
 
-Route::rule('message/checkMessage/:author/:content', '@index/@index/message/checkMessage', 'post');
+Route::rule('message/checkMessage', '@index/@index/message/checkMessage', 'post');
 
 Route::rule('message/delMessage/:id', '@index/message/checkMessage', 'post');
 
-Route::rule('praise/checkPraise/:id/:author/:title', '@index/praise/checkPraise', 'post');
+Route::rule('praise/checkPraise', '@index/praise/checkPraise', 'post');
 
-Route::rule('praise/delPraise/:article_id/:id', '@index/praise/delPraise', 'post');
+Route::rule('praise/delPraise', '@index/praise/delPraise', 'post');
 
 Route::rule('receive/checkPraise/:id', '@index/receive/checkPraise', 'post');
 
 Route::rule('receive/delReceive/:id', '@index/receive/delReceive', 'post');
 
-Route::rule('share/checkShare/:id/:author/:title', '@index/share/checkShare', 'post');
+Route::rule('share/checkShare', '@index/share/checkShare', 'post');
 
-Route::rule('share/delShare/:article_id/:id', '@index/share/delShare', 'post', ['id' => '\d+', 'id' => '\d+']);
+Route::rule('share/delShare', '@index/share/delShare', 'post', ['id' => '\d+', 'id' => '\d+']);
 
 Route::rule('user/checkUsername/:username', '@index/user/checkUsername', 'post');
 
-Route::rule('user/checkRegister/:username/:password', '@index/user/checkRegister', 'post');
+Route::rule('user/checkRegister', '@index/user/checkRegister', 'post');
 
-Route::rule('user/checkLogin/:username/:password', '@index/user/checkLogin', 'post');
-
-Route::rule('user/checkChange/:introduction/:password', '@index/user/checkChange', 'post');
+Route::rule('user/checkChange', '@index/user/checkChange', 'post');
 
 Route::rule('user/defriendUser/:id', '@index/user/defriendUser', 'post');
 
@@ -90,3 +88,29 @@ Route::rule('category/:category', 'category/category');
 Route::rule('announcement/:id', 'announcement/change', 'get');
 
 Route::rule('user/:username', 'user/user');
+
+Route::rule('user/login', 'user/login');
+
+Route::rule('user/checkLogin', 'user/checkLogin','post');
+
+Route::rule('user/register', 'user/register');
+
+Route::rule('user/checkRegister', 'user/checkRegister', 'post');
+
+Route::rule('user/checkLogin', '@index/user/checkLogin', 'post');
+
+Route::rule('user/logout', 'user/logout');
+
+Route::rule('user/change', 'user/change');
+
+Route::rule('user/checkChange', '@index/user/checkChange', 'post');
+
+Route::rule('user/manage', 'user/manage');
+
+Route::rule('user/defriendUser', 'user/defriendUser', 'post');
+
+Route::rule('user/normalUser', 'user/normalUser', 'post');
+
+Route::rule('user/delUser', 'user/delUser', 'post');
+
+Route::rule('category/addCategory', 'category/addCategory', 'post');
