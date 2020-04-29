ad356f753573bcc41f165cccbd78c73a818e510d
shenlink <2024133718@qq.com>
2020-04-29 13:31:16 +0800

    完成了文章表的dataTables的控制器部分

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -96,6 +96,65 @@ class Index extends Base
         return $this->view->fetch('article');
     }
 
+    public function getArticleData()
+    {
+        if (request()->isAjax()) {
+            $article = new Article();
+            //接受请求
+            $datatables = request()->post();
+            //得到排序的方式
+            $order = $datatables['order'][0]['dir'];
+            //得到排序字段的下标
+            $order_column = $datatables['order'][0]['column'];
+            //根据排序字段的下标得到排序字段
+            $order_field = $datatables['columns'][$order_column]['data'];
+            //得到limit参数
+            $limit_start = $datatables['start'];
+            $limit_length = $datatables['length'];
+            //得到搜索的关键词
+            $search = $datatables['search']['value'];
+
+            //如有搜索行为，则按照姓名进行模糊查询
+            if ($search) {
+                $data = $article
+                    ->order("$order_field $order")
+                    ->limit($limit_start, $limit_length)
+                    ->where('title', 'LIKE', "%$search%")
+                    ->select();
+                $keyword_all_data = $article
+                    ->where('title', 'LIKE', "%$search%")
+                    ->select();
+                $total = count($keyword_all_data); //获取满足关键词的总记录数
+            } else {
+
+                //没有关键词，则查询全部
+                $data = $article
+                    ->where('is_delete', 1)
+                    ->field('id,author,title,status,update_time,fans_count,category,comment_count,praise_count,collect_count')
+                    ->order("$order_field $order")
+                    ->limit($limit_start, $limit_length)
+                    ->select();
+                $total = $article->count();
+            }
+
+            if ($data) {
+                $data = collection($data)->toArray();
+            }
+            $draw = request()->post('draw');
+            $AllData = [
+                // ajax的请求次数，创建唯一标识
+                'draw' => $draw,
+                // 结果数
+                'recordsTotal' => count($data),
+                // 总数据量
+                'recordsFiltered' => $total,
+                // 总数据
+                'data' => $data,
+            ];
+            return json($AllData);
+        }
+    }
+
     public function comment()
     {
         $comments = Comment::order('comment_time desc')->paginate(5);
