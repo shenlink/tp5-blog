bd145e1438cd92d976a73babd1116e0257291372
shenlink <2024133718@qq.com>
2020-05-01 18:03:00 +0800

    完成了评论的事务操作

--- application/index/controller/Comment.php
+++ application/index/controller/Comment.php
@@ -5,6 +5,8 @@ namespace app\index\controller;
 use app\index\controller\Base;
 use think\Request;
 use app\index\model\Comment as CommentModel;
+use think\Db;
+
 
 class Comment extends Base
 {
@@ -16,12 +18,24 @@ class Comment extends Base
             $message = '发表失败';
             $data = $request->post();
             $data['username'] = $this->username;
-            $result = CommentModel::insertGetId($data);
-            if ($result != 0) {
+            $article_id = $data['article_id'];
+
+            Db::startTrans();
+            try {
+                $commentResult = Db::table('comment')->insertGetId($data);
+                $articleResult = Db::table('article')->where('id', $article_id)->setInc('comment_count');
+
+                if (!($commentResult && $articleResult)) {
+                    throw new \Exception('发生错误');
+                }
+                Db::commit();
                 $status = 1;
                 $message = '发表成功';
+                return ['status' => $status, 'message' => $message, 'id' => $commentResult];
+            } catch (\Exception $e) {
+                Db::rollback();
+                return ['status' => $status, 'message' => $message];
             }
-            return ['status' => $status, 'message' => $message, 'id' => $result];
         } else {
             return $this->error('非法访问');
         }
@@ -34,12 +48,24 @@ class Comment extends Base
             $status = 0;
             $message = '删除失败';
             $data = $request->post();
-            $result = CommentModel::destroy($data);
-            if ($result == true) {
+            $article_id = $data['article_id'];
+            $id = $data['id'];
+            Db::startTrans();
+            try {
+                $commentResult = Db::table('comment')->where(['id' => $id, 'article_id' => $article_id])->delete();
+                $articleResult = Db::table('article')->where('id', $article_id)->setDec('comment_count');
+
+                if (!($commentResult && $articleResult)) {
+                    throw new \Exception('发生错误');
+                }
+                Db::commit();
                 $status = 1;
                 $message = '删除成功';
+                return ['status' => $status, 'message' => $message];
+            } catch (\Exception $e) {
+                Db::rollback();
+                return ['status' => $status, 'message' => $message];
             }
-            return ['status' => $status, 'message' => $message];
         } else {
             return $this->error('非法访问');
         }
