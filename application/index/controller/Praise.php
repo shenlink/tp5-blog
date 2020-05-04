38f66024ed3b2f09b38db77799f2cb2ed004a2ca
shenlink <2024133718@qq.com>
2020-05-04 19:22:00 +0800

    完善了Praise控制器的注释

--- application/index/controller/Praise.php
+++ application/index/controller/Praise.php
@@ -17,42 +17,55 @@ class Praise extends Base
             $data = $request->post();
             $article_id = $data['article_id'];
             $data['username'] = $this->username;
+            // 确认有没有点赞
             $result = PraiseModel::get(['username' => $this->username, 'article_id' => $article_id]);
+            // 已经点赞了
             if ($result) {
                 $status = -11;
                 $message = '取消失败';
+                // 开启事务
                 Db::startTrans();
                 try {
+                    // 删除点赞记录
                     $praiseResult = PraiseModel::where(['username' => $this->username, 'article_id' => $article_id])->delete();
+                    // Article表的praise_count自减1
                     $articleResult = Article::where('id', $article_id)->setDec('praise_count');
-
+                    // 如果发生错误，就抛出异常
                     if (!($praiseResult && $articleResult)) {
                         throw new \Exception('发生错误');
                     }
+                    // 提交事务
                     Db::commit();
                     $status = 11;
                     $message = '取消成功';
                     return ['status' => $status, 'message' => $message];
                 } catch (\Exception $e) {
+                    // 回滚
                     Db::rollback();
                     return ['status' => $status, 'message' => $message];
                 }
             } else {
                 $status = 0;
                 $message = '点赞失败';
+                // 点赞操作
+                // 开启事务
                 Db::startTrans();
                 try {
+                    // 添加点赞记录
                     $praiseResult = PraiseModel::create($data);
+                    // Article表的praise_count自增1
                     $articleResult = Article::where('id', $article_id)->setInc('praise_count');
-
+                    // 如果发生错误，就抛出异常
                     if (!($praiseResult && $articleResult)) {
                         throw new \Exception('发生错误');
                     }
+                    // 提交事务
                     Db::commit();
                     $status = 1;
                     $message = '点赞成功';
                     return ['status' => $status, 'message' => $message];
                 } catch (\Exception $e) {
+                    // 回滚
                     Db::rollback();
                     return ['status' => $status, 'message' => $message];
                 }
@@ -70,19 +83,24 @@ class Praise extends Base
             $message = '删除失败';
             $data = $request->post();
             $article_id = $data['article_id'];
+            // 开启事务
             Db::startTrans();
             try {
+                // 删除点赞记录
                 $praiseResult = PraiseModel::where(['username' => $this->username, 'article_id' => $article_id])->delete();
+                // Article表的praise_count自减1
                 $articleResult = Article::where('id', $article_id)->setDec('praise_count');
-
+                // 如果发生错误，就抛出异常
                 if (!($praiseResult && $articleResult)) {
                     throw new \Exception('发生错误');
                 }
+                // 提交事务
                 Db::commit();
                 $status = 11;
                 $message = '取消成功';
                 return ['status' => $status, 'message' => $message];
             } catch (\Exception $e) {
+                // 回滚
                 Db::rollback();
                 return ['status' => $status, 'message' => $message];
             }
