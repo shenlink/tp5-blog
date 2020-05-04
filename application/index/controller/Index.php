5b248572355ffae30e74e78243d2137e2288c304
shenlink <2024133718@qq.com>
2020-05-04 18:45:24 +0800

    完善了Index控制器的注释

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -8,11 +8,16 @@ use app\index\model\Article;
 
 class Index extends Base
 {
+    // 展示首页页面
     public function index()
     {
+        // 获取所有的公告内容
         $announcements = Announcement::all();
+        // 获取所有的文章并分页
         $articles = Article::order('update_time desc')->paginate(5);
+        // 获取10篇推荐文章
         $recommends = Article::where('status', 1)->field(['id', 'title'])->limit(10)->order('comment_count', 'desc')->select();
+        // 模板赋值
         $this->view->assign('announcements', $announcements);
         $this->view->assign('articles', $articles);
         $this->view->assign('recommends', $recommends);
