cf1aa0bc409e04c60c9a05a4111a3c39b36e9e90
shenlink <2024133718@qq.com>
2020-05-04 14:04:02 +0800

    使用jquery的ajax方法代替了post方法，代码看起来更规范且易于阅读

--- public/static/js/add.js
+++ public/static/js/add.js
@@ -1,59 +1,79 @@
 // 添加分类
 $('#addCategory').on('click', function () {
     let category = $('#categoryName').val();
-    $.post("/category/checkAddCategory", {
-        category: category
-    }, function (data) {
-        if (data.status === 1) {
-            layer.msg(data.message, {
-                time: 1000
-            }, function () {
-                window.history.back(-1);
-            });
-        } else {
-            layer.msg(data.message, {
-                time: 1000
-            });
+    $.ajax({
+        type: "POST",
+        url: "/category/checkAddCategory",
+        data: {
+            category: category
+        },
+        dataType: "json",
+        success: function (data) {
+            if (data.status == 1) {
+                layer.msg(data.message, {
+                    time: 1000
+                }, function () {
+                    window.history.back(-1);
+                });
+            } else {
+                layer.msg(data.message, {
+                    time: 1000
+                });
+            }
         }
-    },'json');
+    })
 });
 
 // 添加公告
 $('#addAnnouncement').on('click', function () {
     let content = $('#content').val();
-    $.post("/announcement/checkAddAnnouncement", {
-        content: content
-    }, function (data) {
-        if (data.status === 1) {
-            layer.msg(data.message, {
-                time: 1000
-            });
-        } else {
-            layer.msg(data.message, {
-                time: 1000
-            });
+    $.ajax({
+        type: "POST",
+        url: "/announcement/checkAddAnnouncement",
+        data: {
+            content: content
+        },
+        dataType: "json",
+        success: function (data) {
+            if (data.status == 1) {
+                layer.msg(data.message, {
+                    time: 1000
+                }, function () {
+                    window.history.back(-1);
+                });
+            } else {
+                layer.msg(data.message, {
+                    time: 1000
+                });
+            }
         }
-    });
+    })
 });
 
 // 发私信
 $('#addMessage').on('click', function () {
     let username = $('#username').val();
     let content = $('#content').val();
-    $.post("/message/checkMessage", {
-        username: username,
-        content: content
-    }, function (data) {
-        if (data.status === 1) {
-            layer.msg(data.message, {
-                time: 1000
-            }, function () {
-                window.history.back(-1);
-            });
-        } else {
-            layer.msg(data.message, {
-                time: 1000
-            });
+    $.ajax({
+        type: "POST",
+        url: "/message/checkMessage",
+        data: {
+            username: username,
+            content: content
+        },
+        dataType: "json",
+        success: function (data) {
+            if (data.status == 1) {
+                layer.msg(data.message, {
+                    time: 1000
+                }, function () {
+                    window.history.back(-1);
+                });
+            } else {
+                layer.msg(data.message, {
+                    time: 1000
+                });
+            }
         }
-    });
+    })
 });
