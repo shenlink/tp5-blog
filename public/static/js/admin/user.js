ea273a6bf177051d968cfcb5852775b80b378879
shenlink <2024133718@qq.com>
2020-05-04 11:36:48 +0800

    完成了用户表的动态软删除和拉黑，恢复操作

--- public/static/js/admin/user.js
+++ public/static/js/admin/user.js
@@ -8,7 +8,7 @@ function unDeleteAll(middle) {
         if (data.status === 1) {
             layer.msg(data.message, {
                 time: 1000
-            });
+            }, );
         } else {
             layer.msg(data.message, {
                 time: 1000
@@ -27,6 +27,11 @@ function unDelete(middle) {
         if (data.status === 1) {
             layer.msg(data.message, {
                 time: 1000
+            }, function () {
+                var table = $('#dataTable').DataTable();
+                var d = table.row(this).data();
+                d.counter++;
+                table.row(this).data(d).draw();
             });
         } else {
             layer.msg(data.message, {
@@ -53,10 +58,10 @@ function defriendUser(userId) {
             layer.msg(data.message, {
                 time: 1000
             }, function () {
-                let tr = temp.parentNode.parentNode;
-                tr.children[8].innerText = '拉黑';
-                let html = createHtml('normalUser', 'delUser', 'user-id', id, '恢复');
-                tr.lastElementChild.innerHTML = html;
+                var table = $('#dataTable').DataTable();
+                var d = table.row(this).data();
+                d.counter++;
+                table.row(this).data(d).draw();
             });
         } else {
             layer.msg(data.message, {
@@ -77,10 +82,10 @@ function normalUser(userId) {
             layer.msg(data.message, {
                 time: 1000
             }, function () {
-                let tr = temp.parentNode.parentNode;
-                tr.children[8].innerText = '正常';
-                let html = createHtml('defriendUser', 'delUser', 'user-id', id, '拉黑');
-                tr.lastElementChild.innerHTML = html;
+                var table = $('#dataTable').DataTable();
+                var d = table.row(this).data();
+                d.counter++;
+                table.row(this).data(d).draw();
             });
         } else {
             layer.msg(data.message, {
@@ -110,20 +115,10 @@ function delUser(userId) {
             layer.msg(data.message, {
                 time: 1000
             }, function () {
-                let user_tr = parseInt($("#user").children().length);
-                let current_page = parseInt($("#userCurrent").data('current'));
-                let pageCount = parseInt($("#userCurrent").data('page-count'));
-                if (current_page < pageCount) {
-                    window.location.reload();
-                }
-                if (current_page == pageCount && user_tr > 1) {
-                    let tr = temp.parentNode.parentNode;
-                    let tbody = tr.parentNode;
-                    tbody.removeChild(tr);
-                }
-                if (current_page > 1 && current_page == pageCount && user_tr == 1) {
-                    window.location.href = '/admin/manage/aricle/' + (current_page - 1) + '.html';
-                }
+                var table = $('#dataTable').DataTable();
+                var d = table.row(this).data();
+                d.counter++;
+                table.row(this).data(d).draw();
             });
         } else {
             layer.msg(data.message, {
