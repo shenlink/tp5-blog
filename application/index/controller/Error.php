d8dee27da4a8afd54b9369e952f985f1022fc5ba
shenlink <2024133718@qq.com>
2020-05-04 18:29:05 +0800

    完善了Error控制器的注释

--- application/index/controller/Error.php
+++ application/index/controller/Error.php
@@ -2,13 +2,16 @@
 
 namespace app\index\controller;
 
+// 当用户访问不存在的控制器时，定位到这个类
 class Error
 {
+    // 当用户访问不存在的控制器时，定位到这个类的这个方法
     public function index()
     {
         return 'url地址出错了';
     }
 
+    // 当用户访问不存在的控制器并且还访问方法时，定位到这个类的这个方法
     public function _empty()
     {
         return 'url地址出错了';
