356bfd2ee5340b9b077fdeac51c3712e761edb02
shenlink <2024133718@qq.com>
2020-05-04 19:16:10 +0800

    完善了Message控制器的注释

--- application/index/controller/Message.php
+++ application/index/controller/Message.php
@@ -12,11 +12,14 @@ use think\Db;
 
 class Message extends Base
 {
-    // 发私信
+    // 展示发私信页面
     public function sendMessage(Request $request)
     {
+        // 获取要接收的用户的用户名
         $username = $request->param('username');
+        // 获取10篇推荐文章
         $recommends = Article::where('status', 1)->field(['id', 'title'])->limit(10)->order('comment_count', 'desc')->select();
+        // 模板赋值
         $this->view->assign('author', $username);
         $this->view->assign('recommends', $recommends);
         return $this->view->fetch('public/add');
@@ -29,19 +32,24 @@ class Message extends Base
             $status = 0;
             $message = '发送失败';
             $data = $request->post();
+            // 开启事务
             Db::startTrans();
             try {
+                // 添加私信记录
                 $messageResult = MessageModel::create($data);
+                // 添加私信记录
                 $receiveResult = Receive::create($data);
-
+                // 如果发生错误，就抛出异常
                 if (!($messageResult && $receiveResult)) {
                     throw new \Exception('发生错误');
                 }
+                // 提交事务
                 Db::commit();
                 $status = 1;
                 $message = '关注成功';
                 return ['status' => $status, 'message' => $message];
             } catch (\Exception $e) {
+                // 回滚
                 Db::rollback();
                 return ['status' => $status, 'message' => $message];
             }
@@ -57,6 +65,7 @@ class Message extends Base
             $status = 0;
             $message = '删除失败';
             $id = $request->post('id');
+            // 删除指定id的私信
             $result = MessageModel::destroy($id);
             if ($result == true) {
                 $status = 1;
