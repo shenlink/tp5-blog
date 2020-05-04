129eda34871b5c35998a9967cca26a5d4333f1ef
shenlink <2024133718@qq.com>
2020-05-04 18:06:57 +0800

    完善了Category控制器的注释

--- application/index/controller/Category.php
+++ application/index/controller/Category.php
@@ -9,11 +9,14 @@ use think\Request;
 
 class Category extends Base
 {
-    // 添加分类
+    // 展示添加分类页面
     public function addCategory()
     {
+        // 标志位，会显示添加分类的内容
         $addCategory = 'addCategory';
+        // 获取10篇推荐文章
         $recommends = Article::where('status', 1)->field(['id', 'title'])->limit(10)->order('comment_count', 'desc')->select();
+        // 模板赋值
         $this->view->assign('addCategory', $addCategory);
         $this->view->assign('recommends', $recommends);
         return $this->view->fetch('public/add');
@@ -26,6 +29,7 @@ class Category extends Base
             $status = 0;
             $message = '添加失败';
             $data = $request->post();
+            // 确认添加分类
             $result = CategoryModel::create($data);
             if ($result == true) {
                 $status = 1;
@@ -37,14 +41,20 @@ class Category extends Base
         }
     }
 
+    // 当用户输入/category/+分类名后，会访问这个方法
     public function category($category)
     {
+        // 先确认有没有这个分类
         $result = CategoryModel::get(['category' => $category]);
+        // 分类不存在，定位到错误页面
         if (!$result) {
             $this->error('分类不存在', '/');
         }
+        // 获取该分类下的所有文章
         $articles = Article::where(['category' => $category])->order('create_time desc')->paginate(5);
+        // 获取10篇推荐文章
         $recommends = Article::where('status', 1)->field(['id', 'title'])->limit(10)->order('comment_count', 'desc')->select();
+        // 模板赋值
         $this->view->assign('articles', $articles);
         $this->view->assign('category', $category);
         $this->view->assign('recommends', $recommends);
