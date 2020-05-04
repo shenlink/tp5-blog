6b1950152b33763cc59f483b7f5737dd13b197ee
shenlink <2024133718@qq.com>
2020-05-04 13:19:08 +0800

    把admin模块下的Index控制器里的多个getData方法做了个封装

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -3,11 +3,9 @@
 namespace app\admin\controller;
 
 use app\index\controller\Base;
-use app\index\model\Announcement;
 use app\index\model\Article;
 use app\index\model\Category;
 use app\index\model\Comment;
-use app\index\model\Message;
 use app\index\model\User;
 
 class Index extends Base
@@ -46,59 +44,12 @@ class Index extends Base
     public function getUserData()
     {
         if (request()->isAjax()) {
-            $user = new User();
-            //接受请求
             $datatables = request()->post();
-            //得到排序的方式
-            $order = $datatables['order'][0]['dir'];
-            //得到排序字段的下标
-            $order_column = $datatables['order'][0]['column'];
-            //根据排序字段的下标得到排序字段
-            $order_field = $datatables['columns'][$order_column]['data'];
-            //得到limit参数
-            $limit_start = $datatables['start'];
-            $limit_length = $datatables['length'];
-            //得到搜索的关键词
-            $search = $datatables['search']['value'];
-
-            //如有搜索行为，则按照姓名进行模糊查询
-            if ($search) {
-                $data = $user
-                    ->order("$order_field $order")
-                    ->limit($limit_start, $limit_length)
-                    ->where('username', 'LIKE', "%$search%")
-                    ->select();
-                $keyword_all_data = $user
-                    ->where('username', 'LIKE', "%$search%")
-                    ->select();
-                $total = count($keyword_all_data); //获取满足关键词的总记录数
-            } else {
-
-                //没有关键词，则查询全部
-                $data = $user::withTrashed()
-                    ->field('id,username,role,article_count,follow_count,fans_count,create_time,status')
-                    ->order("$order_field $order")
-                    ->limit($limit_start, $limit_length)
-                    ->select();
-                $total = $user::withTrashed()->count();
-            }
-
-            if ($data) {
-                $data = collection($data)->toArray();
-            }
-            $draw = request()->post('draw');
-            $AllData = [
-                // ajax的请求次数，创建唯一标识
-                'draw' => $draw,
-                // 结果数
-                'recordsTotal' => count($data),
-                // 总数据量
-                'recordsFiltered' => $total,
-                // 总数据
-                'data' => $data,
-            ];
-
-            return json($AllData);
+            $class = 'app\index\model\User';
+            $searchName = 'username';
+            $field = 'id,username,role,article_count,follow_count,fans_count,create_time,status';
+            $data = getAdminData($class, $datatables, $searchName, $field);
+            return $data;
         } else {
             return $this->error('非法访问');
         }
@@ -115,58 +66,12 @@ class Index extends Base
     public function getArticleData()
     {
         if (request()->isAjax()) {
-            $article = new Article();
-            //接受请求
             $datatables = request()->post();
-            //得到排序的方式
-            $order = $datatables['order'][0]['dir'];
-            //得到排序字段的下标
-            $order_column = $datatables['order'][0]['column'];
-            //根据排序字段的下标得到排序字段
-            $order_field = $datatables['columns'][$order_column]['data'];
-            //得到limit参数
-            $limit_start = $datatables['start'];
-            $limit_length = $datatables['length'];
-            //得到搜索的关键词
-            $search = $datatables['search']['value'];
-
-            //如有搜索行为，则按照姓名进行模糊查询
-            if ($search) {
-                $data = $article
-                    ->order("$order_field $order")
-                    ->limit($limit_start, $limit_length)
-                    ->where('title', 'LIKE', "%$search%")
-                    ->select();
-                $keyword_all_data = $article
-                    ->where('title', 'LIKE', "%$search%")
-                    ->select();
-                $total = count($keyword_all_data); //获取满足关键词的总记录数
-            } else {
-
-                //没有关键词，则查询全部
-                $data = $article
-                    ->field('id,author,title,status,update_time,category,comment_count,praise_count,collect_count,share_count')
-                    ->order("$order_field $order")
-                    ->limit($limit_start, $limit_length)
-                    ->select();
-                $total = $article->count();
-            }
-
-            if ($data) {
-                $data = collection($data)->toArray();
-            }
-            $draw = request()->post('draw');
-            $AllData = [
-                // ajax的请求次数，创建唯一标识
-                'draw' => $draw,
-                // 结果数
-                'recordsTotal' => count($data),
-                // 总数据量
-                'recordsFiltered' => $total,
-                // 总数据
-                'data' => $data,
-            ];
-            return json($AllData);
+            $class = 'app\index\model\Article';
+            $searchName = 'title';
+            $field = 'id,author,title,status,update_time,category,comment_count,praise_count,collect_count,share_count';
+            $data = getAdminData($class, $datatables, $searchName, $field);
+            return $data;
         } else {
             return $this->error('非法访问');
         }
@@ -182,62 +87,13 @@ class Index extends Base
 
     public function getCommentData()
     {
-        if ($this->isNotAdmin()) {
-            return $this->error('你不是管理员');
-        }
         if (request()->isAjax()) {
-            $comment = new Comment();
-            //接受请求
             $datatables = request()->post();
-            //得到排序的方式
-            $order = $datatables['order'][0]['dir'];
-            //得到排序字段的下标
-            $order_column = $datatables['order'][0]['column'];
-            //根据排序字段的下标得到排序字段
-            $order_field = $datatables['columns'][$order_column]['data'];
-            //得到limit参数
-            $limit_start = $datatables['start'];
-            $limit_length = $datatables['length'];
-            //得到搜索的关键词
-            $search = $datatables['search']['value'];
-
-            //如有搜索行为，则按照姓名进行模糊查询
-            if ($search) {
-                $data = $comment
-                    ->order("$order_field $order")
-                    ->limit($limit_start, $limit_length)
-                    ->where('content', 'LIKE', "%$search%")
-                    ->select();
-                $keyword_all_data = $comment
-                    ->where('content', 'LIKE', "%$search%")
-                    ->select();
-                $total = count($keyword_all_data); //获取满足关键词的总记录数
-            } else {
-
-                //没有关键词，则查询全部
-                $data = $comment
-                    ->field('id,content,comment_time,article_id,title,username')
-                    ->order("$order_field $order")
-                    ->limit($limit_start, $limit_length)
-                    ->select();
-                $total = $comment->count();
-            }
-
-            if ($data) {
-                $data = collection($data)->toArray();
-            }
-            $draw = request()->post('draw');
-            $AllData = [
-                // ajax的请求次数，创建唯一标识
-                'draw' => $draw,
-                // 结果数
-                'recordsTotal' => count($data),
-                // 总数据量
-                'recordsFiltered' => $total,
-                // 总数据
-                'data' => $data,
-            ];
-            return json($AllData);
+            $class = 'app\index\model\Comment';
+            $searchName = 'content';
+            $field = 'id,content,comment_time,article_id,title,username';
+            $data = getAdminData($class, $datatables, $searchName, $field);
+            return $data;
         } else {
             return $this->error('非法访问');
         }
@@ -254,58 +110,12 @@ class Index extends Base
     public function getCategoryData()
     {
         if (request()->isAjax()) {
-            $category = new Category();
-            //接受请求
             $datatables = request()->post();
-            //得到排序的方式
-            $order = $datatables['order'][0]['dir'];
-            //得到排序字段的下标
-            $order_column = $datatables['order'][0]['column'];
-            //根据排序字段的下标得到排序字段
-            $order_field = $datatables['columns'][$order_column]['data'];
-            //得到limit参数
-            $limit_start = $datatables['start'];
-            $limit_length = $datatables['length'];
-            //得到搜索的关键词
-            $search = $datatables['search']['value'];
-
-            //如有搜索行为，则按照姓名进行模糊查询
-            if ($search) {
-                $data = $category
-                    ->order("$order_field $order")
-                    ->limit($limit_start, $limit_length)
-                    ->where('category', 'LIKE', "%$search%")
-                    ->select();
-                $keyword_all_data = $category
-                    ->where('category', 'LIKE', "%$search%")
-                    ->select();
-                $total = count($keyword_all_data); //获取满足关键词的总记录数
-            } else {
-
-                //没有关键词，则查询全部
-                $data = $category
-                    ->field('id,category,article_count,create_time')
-                    ->order("$order_field $order")
-                    ->limit($limit_start, $limit_length)
-                    ->select();
-                $total = $category->count();
-            }
-
-            if ($data) {
-                $data = collection($data)->toArray();
-            }
-            $draw = request()->post('draw');
-            $AllData = [
-                // ajax的请求次数，创建唯一标识
-                'draw' => $draw,
-                // 结果数
-                'recordsTotal' => count($data),
-                // 总数据量
-                'recordsFiltered' => $total,
-                // 总数据
-                'data' => $data,
-            ];
-            return json($AllData);
+            $class = 'app\index\model\Category';
+            $searchName = 'category';
+            $field = 'id,category,article_count,create_time';
+            $data = getAdminData($class, $datatables, $searchName, $field);
+            return $data;
         } else {
             return $this->error('非法访问');
         }
@@ -322,58 +132,12 @@ class Index extends Base
     public function getAnnouncementData()
     {
         if (request()->isAjax()) {
-            $announcement = new Announcement();
-            //接受请求
             $datatables = request()->post();
-            //得到排序的方式
-            $order = $datatables['order'][0]['dir'];
-            //得到排序字段的下标
-            $order_column = $datatables['order'][0]['column'];
-            //根据排序字段的下标得到排序字段
-            $order_field = $datatables['columns'][$order_column]['data'];
-            //得到limit参数
-            $limit_start = $datatables['start'];
-            $limit_length = $datatables['length'];
-            //得到搜索的关键词
-            $search = $datatables['search']['value'];
-
-            //如有搜索行为，则按照姓名进行模糊查询
-            if ($search) {
-                $data = $announcement
-                    ->order("$order_field $order")
-                    ->limit($limit_start, $limit_length)
-                    ->where('content', 'LIKE', "%$search%")
-                    ->select();
-                $keyword_all_data = $announcement
-                    ->where('content', 'LIKE', "%$search%")
-                    ->select();
-                $total = count($keyword_all_data); //获取满足关键词的总记录数
-            } else {
-
-                //没有关键词，则查询全部
-                $data = $announcement
-                    ->field('id,content,create_time')
-                    ->order("$order_field $order")
-                    ->limit($limit_start, $limit_length)
-                    ->select();
-                $total = $announcement->count();
-            }
-
-            if ($data) {
-                $data = collection($data)->toArray();
-            }
-            $draw = request()->post('draw');
-            $AllData = [
-                // ajax的请求次数，创建唯一标识
-                'draw' => $draw,
-                // 结果数
-                'recordsTotal' => count($data),
-                // 总数据量
-                'recordsFiltered' => $total,
-                // 总数据
-                'data' => $data,
-            ];
-            return json($AllData);
+            $class = 'app\index\model\Announcement';
+            $searchName = 'content';
+            $field = 'id,content,create_time';
+            $data = getAdminData($class, $datatables, $searchName, $field);
+            return $data;
         } else {
             return $this->error('非法访问');
         }
@@ -390,58 +154,12 @@ class Index extends Base
     public function getMessageData()
     {
         if (request()->isAjax()) {
-            $message = new Message();
-            //接受请求
             $datatables = request()->post();
-            //得到排序的方式
-            $order = $datatables['order'][0]['dir'];
-            //得到排序字段的下标
-            $order_column = $datatables['order'][0]['column'];
-            //根据排序字段的下标得到排序字段
-            $order_field = $datatables['columns'][$order_column]['data'];
-            //得到limit参数
-            $limit_start = $datatables['start'];
-            $limit_length = $datatables['length'];
-            //得到搜索的关键词
-            $search = $datatables['search']['value'];
-
-            //如有搜索行为，则按照姓名进行模糊查询
-            if ($search) {
-                $data = $message
-                    ->order("$order_field $order")
-                    ->limit($limit_start, $limit_length)
-                    ->where('username', 'LIKE', "%$search%")
-                    ->select();
-                $keyword_all_data = $message
-                    ->where('username', 'LIKE', "%$search%")
-                    ->select();
-                $total = count($keyword_all_data); //获取满足关键词的总记录数
-            } else {
-
-                //没有关键词，则查询全部
-                $data = $message
-                    ->field('id,username,content,message_time')
-                    ->order("$order_field $order")
-                    ->limit($limit_start, $limit_length)
-                    ->select();
-                $total = $message->count();
-            }
-
-            if ($data) {
-                $data = collection($data)->toArray();
-            }
-            $draw = request()->post('draw');
-            $AllData = [
-                // ajax的请求次数，创建唯一标识
-                'draw' => $draw,
-                // 结果数
-                'recordsTotal' => count($data),
-                // 总数据量
-                'recordsFiltered' => $total,
-                // 总数据
-                'data' => $data,
-            ];
-            return json($AllData);
+            $class = 'app\index\model\Message';
+            $searchName = 'username';
+            $field = 'id,username,content,message_time';
+            $data = getAdminData($class, $datatables, $searchName, $field);
+            return $data;
         } else {
             return $this->error('非法访问');
         }
--- application/common.php
+++ application/common.php
@@ -18,3 +18,83 @@ function truncate($text, $length)
     }
     return $text;
 }
+
+// 获取管理页面的数据
+function getAdminData($class, $datatables, $searchName, $field)
+{
+    $instance = new $class();
+    //接受请求
+    $datatables = request()->post();
+    //得到排序的方式
+    $order = $datatables['order'][0]['dir'];
+    //得到排序字段的下标
+    $order_column = $datatables['order'][0]['column'];
+    //根据排序字段的下标得到排序字段
+    $order_field = $datatables['columns'][$order_column]['data'];
+    //得到limit参数
+    $limit_start = $datatables['start'];
+    $limit_length = $datatables['length'];
+    //得到搜索的关键词
+    $search = $datatables['search']['value'];
+
+    //如有搜索行为，则按照姓名进行模糊查询
+
+    if ($search) {
+        if ($class == 'app\index\model\User') {
+            $data = $instance::withTrashed()
+                ->order("$order_field $order")
+                ->limit($limit_start, $limit_length)
+                ->where($searchName, 'LIKE', "%$search%")
+                ->select();
+            $keyword_all_data = $instance::withTrashed()
+                ->where($searchName, 'LIKE', "%$search%")
+                ->select();
+            $total = count($keyword_all_data); //获取满足关键词的总记录数
+        } else {
+            $data = $instance
+                ->order("$order_field $order")
+                ->limit($limit_start, $limit_length)
+                ->where($searchName, 'LIKE', "%$search%")
+                ->select();
+            $keyword_all_data = $instance
+                ->where($searchName, 'LIKE', "%$search%")
+                ->select();
+            $total = count($keyword_all_data); //获取满足关键词的总记录数
+        }
+    } else {
+        if ($class == 'app\index\model\User') {
+            //没有关键词，则查询全部
+            $data = $instance::withTrashed()
+                ->field($field)
+                ->order("$order_field $order")
+                ->limit($limit_start, $limit_length)
+                ->select();
+            $total = $instance::withTrashed()->count();
+        } else {
+            //没有关键词，则查询全部
+            $data = $instance
+                ->field($field)
+                ->order("$order_field $order")
+                ->limit($limit_start, $limit_length)
+                ->select();
+            $total = $instance->count();
+        }
+    }
+
+    if ($data) {
+        $data = collection($data)->toArray();
+    }
+    $draw = request()->post('draw');
+    $AllData = [
+        // ajax的请求次数，创建唯一标识
+        'draw' => $draw,
+        // 结果数
+        'recordsTotal' => count($data),
+        // 总数据量
+        'recordsFiltered' => $total,
+        // 总数据
+        'data' => $data,
+    ];
+
+    return json($AllData);
+}
