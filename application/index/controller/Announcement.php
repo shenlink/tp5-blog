0cda7093f0c8242efa37a0bceefbe125079bd1bd
shenlink <2024133718@qq.com>
2020-05-04 16:31:29 +0800

    完善了注释

--- application/index/controller/Announcement.php
+++ application/index/controller/Announcement.php
@@ -9,23 +9,27 @@ use app\index\model\Announcement as AnnouncementModel;
 
 class Announcement extends Base
 {
-    // 添加公告
+    // 展示添加公告页面
     public function add()
     {
+        // 标志位，会显示添加公告的内容
         $addAnnouncement = 'addAnnouncement';
+        // 获取10篇推荐文章
         $recommends = Article::where('status', 1)->field(['id', 'title'])->limit(10)->order('comment_count', 'desc')->select();
+        // 模板赋值
         $this->view->assign('addAnnouncement', $addAnnouncement);
         $this->view->assign('recommends', $recommends);
         return $this->view->fetch('public/add');
     }
 
-    // 确认添加
+    // 确认添加公告
     public function checkAddAnnouncement(Request $request)
     {
         if (request()->isAjax()) {
             $status = 0;
             $message = '添加失败';
             $data = $request->post();
+            // 添加公告进数据库
             $result = AnnouncementModel::create($data);
             if ($result == true) {
                 $status = 1;
@@ -37,13 +41,18 @@ class Announcement extends Base
         }
     }
 
-    // 修改公告
+    // 展示修改公告页面
     public function change(Request $request)
     {
+        // 标志位，会显示修改公告的内容
         $changeAnnouncement = 'changeAnnouncement';
+        // 获取当前的公告id
         $id = $request->param('id');
+        // 获取当前的公告内容
         $announcements = AnnouncementModel::get(['id', $id]);
+        // 获取10篇推荐文章
         $recommends = Article::where('status', 1)->field(['id', 'title'])->limit(10)->order('comment_count', 'desc')->select();
+        // 模板赋值
         $this->view->assign('announcements', $announcements);
         $this->view->assign('changeAnnouncement', $changeAnnouncement);
         $this->view->assign('recommends', $recommends);
@@ -51,7 +60,7 @@ class Announcement extends Base
         return $this->view->fetch('public/change');
     }
 
-    // 确认修改
+    // 确认修改公告
     public function checkChange(Request $request)
     {
         if (request()->isAjax()) {
@@ -59,6 +68,7 @@ class Announcement extends Base
             $message = '修改失败';
             $data = $request->post();
             $condition = ['id' => $data['id']];
+            // 更新该条公告
             $result = AnnouncementModel::update($data, $condition);
             if ($result == true) {
                 $status = 1;
@@ -77,6 +87,7 @@ class Announcement extends Base
             $status = 0;
             $message = '删除失败';
             $id = $request->post('id');
+            // 删除该条公告
             $result = AnnouncementModel::destroy($id);
             if ($result == true) {
                 $status = 1;
