74fa52b77ecee84cfc78166485de087a7d5dac14
shenlink <2024133718@qq.com>
2020-05-04 19:39:34 +0800

    完善了Share控制器的注释

--- application/index/controller/Share.php
+++ application/index/controller/Share.php
@@ -17,42 +17,55 @@ class Share extends Base
             $data = $request->post();
             $article_id = $data['article_id'];
             $data['username'] = $this->username;
+            // 确认有没有分享
             $result = ShareModel::get(['username' => $this->username, 'article_id' => $article_id]);
+            // 已经分享了
             if ($result) {
                 $status = -11;
                 $message = '取消失败';
+                // 开启事务
                 Db::startTrans();
                 try {
+                    // 删除分享记录
                     $shareResult = ShareModel::where(['username' => $this->username, 'article_id' => $article_id])->delete();
+                    // Article表的share_count自减1
                     $articleResult = Article::where('id', $article_id)->setDec('share_count');
-
+                    // 如果发生错误，就抛出异常
                     if (!($shareResult && $articleResult)) {
                         throw new \Exception('发生错误');
                     }
+                    // 提交事务
                     Db::commit();
                     $status = 11;
                     $message = '取消成功';
                     return ['status' => $status, 'message' => $message];
                 } catch (\Exception $e) {
+                    // 回滚
                     Db::rollback();
                     return ['status' => $status, 'message' => $message];
                 }
             } else {
                 $status = 0;
                 $message = '分享失败';
+                // 分享操作
+                // 开启事务
                 Db::startTrans();
                 try {
+                    // 添加分享记录
                     $shareResult = ShareModel::insert($data);
+                    // Article表的share_count自增1
                     $articleResult = Article::where('id', $article_id)->setInc('share_count');
-
+                    // 如果发生错误，就抛出异常
                     if (!($shareResult && $articleResult)) {
                         throw new \Exception('发生错误');
                     }
+                    // 提交事务
                     Db::commit();
                     $status = 1;
                     $message = '分享成功';
                     return ['status' => $status, 'message' => $message];
                 } catch (\Exception $e) {
+                    // 回滚
                     Db::rollback();
                     return ['status' => $status, 'message' => $message];
                 }
@@ -70,19 +83,24 @@ class Share extends Base
             $message = '删除失败';
             $data = $request->post();
             $article_id = $data['article_id'];
+            // 开启事务
             Db::startTrans();
             try {
+                // 删除分享记录
                 $shareResult = ShareModel::where(['username' => $this->username, 'article_id' => $article_id])->delete();
+                // Article表的share_count自增1
                 $articleResult = Article::where('id', $article_id)->setDec('share_count');
-
+                // 如果发生错误，就抛出异常
                 if (!($shareResult && $articleResult)) {
                     throw new \Exception('发生错误');
                 }
+                // 提交事务
                 Db::commit();
                 $status = 1;
                 $message = '删除成功';
                 return ['status' => $status, 'message' => $message];
             } catch (\Exception $e) {
+                // 回滚
                 Db::rollback();
                 return ['status' => $status, 'message' => $message];
             }
